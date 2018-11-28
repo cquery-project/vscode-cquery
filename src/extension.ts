4129c8cd29aa940decd8361849ae8f63149defb5
Nathan Ridge <zeratul976@hotmail.com>
2018-11-27 23:39:09 -0500

    Forward LD_LIBRARY_PATH to the server (fixes #61)

--- src/extension.ts
+++ src/extension.ts
@@ -249,6 +249,7 @@ export function activate(context: ExtensionContext) {
     let kToForward = [
       'ProgramData',
       'PATH',
+      'LD_LIBRARY_PATH',
     ];
     for (let e of kToForward)
       env[e] = process.env[e];
